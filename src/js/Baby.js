14a03935e70627efa23ded6e6664a363d0204228
HaiXuan123 <2269287394@qq.com>
2017-08-07 08:02:37 +0800

    园所概况和baby页面管理

--- src/js/Baby.js
+++ src/js/Baby.js
@@ -6,9 +6,165 @@ import $ from 'jquery';
 
 
 class Baby extends Component{
+    constructor(){
+        super();
+        this.state={
+            model:[{"id":"","txt":""}],
+            model1:[{"id":"","txt":""}],
+            uid:"",
+
+            park:[{"id":"","txt":""}],
+            park1:[{"id":"","txt":""}],
+            pid:""
+        }
+    }
+    componentDidMount(){
+        // model文字调取开始
+        $.ajax({
+            url:'http://192.168.43.5:8005/model',
+            type:'get',
+            success:function(b){
+                console.log(b);
+                this.setState({model1:b});
+            }.bind(this)
+        });
+        // model文字调取结束
+        // {/*.亲子主题乐园文字开始*/}
+        $.ajax({
+            url:'http://192.168.43.5:8005/baby_park',
+            type:'get',
+            success:function(b){
+                console.log(b);
+                this.setState({park1:b});
+            }.bind(this)
+        });
+        // {/*.亲子主题乐园文字完*/}
+    }
+    //model文字修改
+//     修改按钮
+    modelClick=function(event) {
+        $(".listCourse").css("display", "block");
+        var aa = event.target;
+        var id = aa.parentElement.parentElement.firstElementChild.innerHTML;
+        this.setState({
+            uid: id,
+        })
+    }.bind(this)
+    // 确定按钮    学校环境文字修改
+    confirmfn(){
+        $(".listCourse").css("display", "none");
+        var txt = $(".listCourse input:nth-of-type(1)").val();
+
+
+        if (txt== "") {
+            alert("不能为空")
+        } else {
+            $.ajax({
+                type: "post",
+                url: "http://192.168.43.5:8005/model/babyModel",
+                data: {
+                    id: this.state.uid,
+                    txt: txt,
+                },
+                success: (e)=> {
+                    this.setState({
+                        model: e
+                    })
+                },
+                error: function () {
+                    console.log("失败")
+                }
+            });
+        }
+    }
+//model文字修改完
+
+    //park文字修改
+//     修改按钮
+    parkClick=function(event) {
+        $(".listCourse1").css("display", "block");
+        var aa = event.target;
+        var id = aa.parentElement.parentElement.firstElementChild.innerHTML;
+        this.setState({
+            pid: id,
+        })
+    }.bind(this)
+    // 确定按钮    学校环境文字修改
+    confirmfn1(){
+        $(".listCourse1").css("display", "none");
+        var txt = $(".listCourse1 input:nth-of-type(1)").val();
+
+
+        if (txt=="") {
+            alert("不能为空")
+        } else {
+            $.ajax({
+                type: "post",
+                url: "http://192.168.43.5:8005/baby_park/parkModel",
+                data: {
+                    id: this.state.pid,
+                    txt: txt,
+                },
+                success: (e)=> {
+                    this.setState({
+                        park: e
+                    })
+                },
+                error: function () {
+                    console.log("失败")
+                }
+            });
+        }
+    }
+//park文字修改完
     render(){
         return (
-            <div className="BabyWrap"></div>
+            <div className="BabyWrap">
+                {/*.model开始*/}
+                <div className="modelWrap">
+                    <h3>小孩文字</h3>
+                    <ul>
+                        <li>id</li>
+                        <li>txt</li>
+                    </ul>
+                    {this.state.model1.map((con,i)=>{
+                        return <ul key={i}>
+                                <li>{con.id}</li>
+                                <li>{con.txt}</li>
+                                <li>
+                                    <button onClick={this.modelClick}>修改</button>
+                                </li>
+                            </ul>
+                    })}
+                    <div className="listCourse">
+                        <input type="text" placeholder="txt"/>
+                        <button id="confirm" onClick={this.confirmfn.bind(this)}>确定</button>
+                    </div>
+                </div>
+                {/*model结束*/}
+                {/*.亲子主题乐园文字开始*/}
+                <div className="parkWrap">
+                    <h3>亲子主题乐园文字</h3>
+                    <ul>
+                        <li>id</li>
+                        <li>txt</li>
+                    </ul>
+                    {this.state.park1.map((con,i)=>{
+                        return <ul key={i}>
+                            <li>{con.id}</li>
+                            <li>{con.txt}</li>
+                            <li>
+                                <button onClick={this.parkClick}>修改</button>
+                            </li>
+                        </ul>
+                    })}
+                    <div className="listCourse1">
+                        <input type="text" placeholder="txt"/>
+                        <button id="confirm1" onClick={this.confirmfn1.bind(this)}>确定</button>
+                    </div>
+                </div>
+                {/*亲子主题乐园文字结束*/}
+            </div>
         )
     }
 }
--- src/js/Garden.js
+++ src/js/Garden.js
@@ -18,8 +18,13 @@ constructor() {
         uid:"",
 
         picture:[{"id":"","src":""}],
-        // picture1:[{"id":"","src":""}],
-        pid:""
+        pid:null,
+
+        introduce:[{"id":"","src":"","title":"","enTitleA":"","enTitleB":"","conA":"","conB":"","conC":"","btn":""}],
+        introduce1:[{"id":"","src":"","title":"","enTitleA":"","enTitleB":"","conA":"","conB":"","conC":"","btn":""}],
+        inid:"",
+
+
 
     }
 }
@@ -61,48 +66,6 @@ constructor() {
             }
         })
     }.bind(this);
-// banner修改图片完
-
-    // // picture修改图片
-    setimg1 = function (element) {
-        console.log(element);
-        var files = [];
-        files = element.files[0];
-        var fd = new FormData();  //表单处理数据的方法
-        fd.append('uploadedFile', files);
-        //用append方法以键值对的方式保存
-        console.log(fd);
-        $.ajax({
-            type: "post",
-            url: "http://192.168.43.5:8005/conB_picture/conb_pic",
-            async: true,
-            data: fd,
-            contentType: false,
-            processData: false,
-            success: function (e) {
-                console.log(666)
-
-                $.ajax({
-                    type: "post",
-                    url: "http://192.168.43.5:8005/conB_picture/conb_picUp",
-                    data: {"id": this.state.aid},
-                    success: function (e) {
-                        alert(e)
-
-                    }.bind(this),
-                    error: function () {
-                        console.log("修改失败")
-                    }
-                });
-
-            }.bind(this),
-            error:function () {
-                alert("上传失败")
-            }
-        })
-    }.bind(this);
-
-
 // picture修改图片完
 componentDidMount() {
     // banner
@@ -142,8 +105,7 @@ componentDidMount() {
     //学校环境文字调取完
 
 
-    // 照片墙图
-    //上传图片
+    // 照片墙图片获取
     $.ajax({
         url: 'http://192.168.43.5:8005/conB_picture/get',
         type: 'get',
@@ -153,22 +115,105 @@ componentDidMount() {
             })
         }.bind(this)
     });
-    var pictureBWrap= document.getElementById('pictureBWrap');
-    pictureBWrap.onclick = function (e) {
 
-        var ev = e || window.event;
-        var target = ev.target || ev.srcElement;
-        if (target.innerHTML == "修改") {
-            var id = target.parentNode.parentNode.children[0].innerHTML;
-            this.setState({
-                pid: id
-            });
-            $('.updateBox1').css('display', 'block');
-        }
-    }.bind(this);
-    //上传图片完
-    // 照片墙完
+    // 介绍
+    $.ajax({
+        url:'http://192.168.43.5:8005/conB_introduce',
+        type:'get',
+        success:function(a){
+            console.log(a);
+            this.setState({introduce1:a});
+        }.bind(this)
+    });
+
+    // 介绍完
 }
+
+
+
+    //照片墙图片修改
+    setimg1 = function (element) {
+        console.log(element);
+        var files = [];
+        files = element.files[0];
+        var fd = new FormData();  //表单处理数据的方法
+        fd.append('uploadedFile', files)
+        //用append方法以键值对的方式保存
+        console.log(fd);
+        $.ajax({
+            type: "post",
+            url: "http://192.168.43.5:8005/conB_picture/conb_pic",
+            async: true,
+            data: fd,
+            contentType: false,
+            processData: false,
+            success: function (e) {
+                $.ajax({
+                    type: "post",
+                    url: "http://192.168.43.5:8005/conB_picture/conb_picUp",
+                    data: {"id": this.state.pid},
+                    success: function (e) {
+                        alert(e)
+
+                    }.bind(this),
+                    error: function () {
+                        console.log("修改失败")
+                    }
+                });
+            }.bind(this),
+            error: function () {
+                alert("上传失败")
+            }
+        });
+    }.bind(this);
+    //照片墙图片修改完
+    //照片墙图片修改
+    setimg2 = function (element) {
+        console.log(element);
+        var files = [];
+        files = element.files[0];
+        var fd = new FormData();  //表单处理数据的方法
+        fd.append('uploadedFile', files)
+        //用append方法以键值对的方式保存
+        console.log(fd);
+        $.ajax({
+            type: "post",
+            url: "http://192.168.43.5:8005/conB_introduce/conB_introduceImg",
+            async: true,
+            data: fd,
+            contentType: false,
+            processData: false,
+            success: function (e) {
+                $.ajax({
+                    type: "post",
+                    url: "http://192.168.43.5:8005/conB_introduce/conB_introduceImgUp",
+                    data: {"id": this.state.inid},
+                    success: function (e) {
+                        alert(e)
+
+                    }.bind(this),
+                    error: function () {
+                        console.log("修改失败")
+                    }
+                });
+            }.bind(this),
+            error: function () {
+                alert("上传失败")
+            }
+        });
+    }.bind(this);
+   //照片墙图片修改完
+    picClick=function(event) {
+        $(".updateBox1").css("display", "block");
+        var dd = event.target;
+        var id = dd.parentElement.parentElement.firstElementChild.innerHTML;
+        console.log(id)
+        this.setState({
+            pid: id,
+        })
+    }.bind(this);
+
+
 //学校环境文字修改
 //     修改按钮
     conEnvir=function(event) {
@@ -179,7 +224,7 @@ componentDidMount() {
             uid: id,
         })
     }.bind(this);
-    // 确定按钮
+    // 确定按钮    学校环境文字修改
     confirmfn(){
     $(".listCourse").css("display", "none");
     var title = $(".listCourse input:nth-of-type(1)").val();
@@ -219,6 +264,62 @@ componentDidMount() {
 //学校环境文字修改完
 
 
+//介绍文字修改
+//     修改按钮
+    conIntroduce=function(event) {
+        $(".listIntroduce").css("display", "block");
+        var aa = event.target;
+        var id = aa.parentElement.parentElement.firstElementChild.innerHTML;
+        this.setState({
+            inid: id,
+        })
+    }.bind(this);
+    // 确定按钮    学校环境文字修改
+    confirmfn1(){
+        $(".listIntroduce").css("display", "none");
+        // var src = $(".listCourse input:nth-of-type(1)").val();
+        var title = $(".listIntroduce input:nth-of-type(2)").val();
+        var enTitleA = $(".listIntroduce input:nth-of-type(3)").val();
+        var enTitleB = $(".listIntroduce input:nth-of-type(4)").val();
+        var conA = $(".listIntroduce input:nth-of-type(5)").val();
+        var conB = $(".listIntroduce input:nth-of-type(6)").val();
+        var conC = $(".listIntroduce input:nth-of-type(7)").val();
+        var btn = $(".listIntroduce input:nth-of-type(8)").val();
+
+
+
+
+
+        if (title == ""||enTitleA==""||enTitleB==""||conA==""||conB==""||conC==""||btn=="") {
+            alert("不能为空")
+        } else {
+            $.ajax({
+                type: "post",
+                url: "http://192.168.43.5:8005/conB_introduce/conB_introduce1",
+                data: {
+                    id: this.state.inid,
+                    // src:src,
+                    title: title,
+                    enTitleA: enTitleA,
+                    enTitleB: enTitleB,
+                    conA: conA,
+                    conB: conB,
+                    conC: conC,
+                    btn: btn,
+                },
+
+                success: (e)=> {
+                    this.setState({
+                        introduce: e
+                    })
+                },
+                error: function () {
+                    console.log("失败")
+                }
+            });
+        }
+    }
+//介绍文字修改完
 render() {
     return (
         <div className="gardenWrap">
@@ -231,15 +332,15 @@ render() {
             {/*栏目完*/}
             {/*banner*/}
             <div id="bannerBWrap">
-                {this.state.picture.map(function (v, i) {
+                {this.state.banner.map(function (v, i) {
                     return <ul key={i} className="bannerB">
                         <li>{v.id}</li>
                         <li><img src={v.src}/></li>
                         <li>
                             <button>修改</button>
                         </li>
-                        <div className="updateBox1">
-                            <input type="file" ref="filaa1" onChange={this.setimg.bind(null, this.refs.filaa1)}/>
+                        <div className="updateBox">
+                            <input type="file" ref="filaa" onChange={this.setimg.bind(null, this.refs.filaa)}/>
                         </div>
                     </ul>
                 }.bind(this))}
@@ -306,16 +407,78 @@ render() {
                     return <ul key={i} className="pictureB">
                         <li>{v.id}</li>
                         <li><img src={v.src}/></li>
-                        <li>
-                            <button>修改</button>
-                        </li>
-                        <div className="updateBox">
-                            <input type="file" ref="filaa1" onChange={this.setimg.bind(null, this.refs.filaa1)}/>
-                        </div>
+                        <li><button onClick={this.picClick}>修改</button></li>
                     </ul>
                 }.bind(this))}
+                <div className="updateBox1">
+                    <input type="file" ref="filaa1" onChange={this.setimg1.bind(this, this.refs.filaa1)}/>
+                </div>
+
             </div>
             {/*照片墙完*/}
+
+            {/*介绍*/}
+            <div id="introduceBWrap">
+                <h3>介绍</h3>
+
+                <ul className="title">
+                    <li>id</li>
+                    <li>src</li>
+                    <li>title</li>
+                    <li>enTitleA</li>
+                    <li>enTitleB</li>
+                    <li>conA</li>
+                    <li>conB</li>
+                    <li>conC</li>
+                    <li>btn</li>
+                </ul>
+
+                {this.state.introduce1.map((e,i)=> {
+                    return <div key={i} className="introduce">
+                        <ul>
+                            <li>{e.id}</li>
+                            <li><button onClick={this.conIntroduce}>修改</button></li>
+                        </ul>
+
+                        <ul>
+                            <li><img src={e.src} alt=""/></li>
+                        </ul>
+                        <ul>
+                            <li>{e.title}</li>
+                        </ul>
+                        <ul>
+                            <li>{e.enTitleA}</li>
+                        </ul>
+                        <ul>
+                            <li>{e.enTitleB}</li>
+                        </ul>
+                        <ul>
+                            <li>{e.conA}</li>
+                        </ul>
+                        <ul>
+                            <li>{e.conB}</li>
+                        </ul>
+                        <ul>
+                            <li>{e.conC}</li>
+                        </ul>
+                        <ul>
+                            <li>{e.btn}</li>
+                        </ul>
+                        <div className="listIntroduce">
+                            <input type="file" ref="filaa2" onChange={this.setimg2.bind(this, this.refs.filaa2)}/>
+                            <input type="text" placeholder="title"/>
+                            <input type="text" placeholder="enTitleA"/>
+                            <input type="text" placeholder="enTitleB"/>
+                            <input type="text" placeholder="conA"/>
+                            <input type="text" placeholder="conB"/>
+                            <input type="text" placeholder="conC"/>
+                            <input type="text" placeholder="btn"/>
+                            <button id="confirm1" onClick={this.confirmfn1.bind(this)}>确定</button>
+                        </div>
+                    </div>
+                })}
+            </div>
+            {/*介绍完*/}
         </div>
     )
 }
